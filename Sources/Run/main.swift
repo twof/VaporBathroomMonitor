63592633df62e6663c3cd9d555f784ac33d5bf03
twof <fabiobean2@gmail.com>
2017-12-23 14:15:25 -0800

    Supporting custom ports for heroku

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -1,10 +1,27 @@
 import App
 import Service
 import Vapor
+import Foundation
 
 var config = Config.default()
 var env = Environment.detect()
 var services = Services.default()
+
+private let commandLineKey = "--port="
+var serverConfig: EngineServerConfig = EngineServerConfig()
+
+for arg in CommandLine.arguments {
+    if arg.hasPrefix(commandLineKey) {
+        var string = arg
+        string.removeFirst(commandLineKey.count)
+        guard let port = UInt16(string) else {continue}
+        serverConfig = EngineServerConfig(port: port)
+    }
+}
+
+services.register { container in
+    return serverConfig
+}
 
 try App.configure(&config, &env, &services)
 
@@ -17,3 +34,6 @@ let app = try Application(
 try App.boot(app)
 
 try app.run()
+
+
+
